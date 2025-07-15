363fdd11c585955a6a794202463566051677a41a
Divy Sharma <sharmadivy99@gmail.com>
2025-07-15 23:16:14 +0530

    Add files via upload

---
+++ MySQLD1.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE startersql;
+USE startersql;
+CREATE TABLE USERS(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('MALE','FEMALE','OTHERS'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+SELECT * FROM users;
